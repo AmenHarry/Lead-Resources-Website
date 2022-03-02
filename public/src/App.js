af9b112794a4208bd1961a427b2cc1db43a9abbb
AmenHarry <98628305+AmenHarry@users.noreply.github.com>
2022-03-02 12:14:18 +0100

    Add files via upload

---
+++ public/src/App.js
@@ -0,0 +1,16 @@
+import Hero from './components/Hero';
+import './App.css';
+import Products from './components/Products';
+import Thinking from './components/Thinking';
+
+function App() {
+  return (
+    <div className="">
+      <Hero />
+      <Products />
+      <Thinking />
+    </div>
+  );
+}
+
+export default App;
